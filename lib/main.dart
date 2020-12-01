77bbec10430eebe15ad86380233cc8cc6719a977
Rene <renealejandrosanchezmorales@gmail.com>
2020-12-01 15:19:27 -0300

    se crea una figura triangular

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:design/src/pages/headers_page.dart';
+import 'package:design/src/widgets/headers.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home: HeadersPage(),
+      home: HeaderTriangular(),
     );
   }
 }
--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -65,3 +65,57 @@ class _HeaderDiagonalPainter extends CustomPainter{
     return true;
   }
 }
+
+class HeaderTriangular extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      // color: Color(0xff615AAB),
+      child: CustomPaint(
+        painter: _HeaderTriangularPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderTriangularPainter extends CustomPainter{
+  @override
+  void paint(Canvas canvas, Size size) {
+
+    //lapiz para pintar y dibujar
+    final paint = new Paint();
+
+    //Propiedades
+    paint.color = Color(0xff615AAB);
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 2;    
+
+    final path = new Path();
+ 
+    //dibujar con el path y el lapiz
+    path.lineTo(size.width, size.height);
+    path.lineTo(size.width, size.height * 0);
+
+    canvas.drawPath(path, paint);
+
+    final path2 = new Path();
+    final paint2 = new Paint();
+
+    //Propiedades
+    paint2.color = Colors.orangeAccent;
+    paint2.style = PaintingStyle.fill;
+    paint2.strokeWidth = 2;
+
+    path2.lineTo(0, size.height);
+    path2.lineTo(size.width, size.height);
+
+    canvas.drawPath(path2, paint2);
+  }
+  
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
