7f9d73ad8dfab36a276d1d01d383940da037c271
Rene <renealejandrosanchezmorales@gmail.com>
2020-12-01 14:48:44 -0300

    se implementa custom pointer para dibujar una figura

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -25,8 +25,43 @@ class HeaderDiagonal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 300,
-      color: Color(0xff615AAB)
+      height: double.infinity,
+      width: double.infinity,
+      // color: Color(0xff615AAB),
+      child: CustomPaint(
+        painter: _HeaderDiagonalPainter(),
+      ),
     );
+  }
+}
+
+class _HeaderDiagonalPainter extends CustomPainter{
+  @override
+  void paint(Canvas canvas, Size size) {
+
+    //lapiz para pintar y dibujar
+    final paint = new Paint();
+  
+
+    //Propiedades
+    paint.color = Color(0xff615AAB);
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 2;
+
+    final path = new Path();
+ 
+    //dibujar con el path y el lapiz
+    path.moveTo(0, size.height * 0.35);
+    path.lineTo(size.width, size.height * 0.30);
+    path.lineTo(size.width, size.height * 0);
+    path.lineTo(0, 0);
+    // path.lineTo(0, size.height * 0.5);
+
+    canvas.drawPath(path, paint);
+  }
+  
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
   }
 }
