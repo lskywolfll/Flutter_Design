640cfc8cd6b9faa0714f9ddd5b872f2bb78989b3
Rene <renealejandrosanchezmorales@gmail.com>
2020-12-03 20:55:10 -0300

    se hace un header curvo donde es bastante faciles hacer cosas curvas tantas veces queramos

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -215,3 +215,45 @@ class _HeaderPicoPainter extends CustomPainter{
     return true;
   }
 }
+
+class HeaderCurvo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      // color: Color(0xff615AAB),
+      child: CustomPaint(
+        painter: _HeaderCurvoPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderCurvoPainter extends CustomPainter{
+  @override
+  void paint(Canvas canvas, Size size) {
+
+    //lapiz para pintar y dibujar
+    final paint = new Paint();
+
+    //Propiedades
+    paint.color = Color(0xff615AAB);
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 20;    
+
+    final path = new Path();
+ 
+    //dibujar con el path y el lapiz
+    path.lineTo(0, size.height * 0.25);
+    path.quadraticBezierTo(size.width * 0.5, size.height * 0.5, size.width, size.height * 0.25);
+    path.lineTo(size.width, 0);
+
+    canvas.drawPath(path, paint);
+  }
+  
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
